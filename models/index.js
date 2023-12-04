39c81b3b2f82291e393f9aa9fbfe1d3792284d13
Kalkidan-Geb <144774340+Kalkidan-Geb@users.noreply.github.com>
2023-12-04 12:24:25 -0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,67 +1,43 @@
-const { Sequelize, DataTypes } = require('sequelize');
+'use strict';
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'path/to/database.sqlite', // Update with your database path
-});
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-const User = require('./user')(sequelize);
-const Course = require('./course')(sequelize);
-
-module.exports = {
-  User,
-  Course,
-  sequelize,
-};
-
-middleware/auth-user.js
-// middleware/auth-user.js
-const { User } = require('../models');
-
-const authenticateUser = async (req, res, next) => {
-  let message;
-  const credentials = requireHeader(req);
-  if (credentials) {
-    const user = await User.findOne({
-      where: {
-        emailAddress: credentials.name,
-      },
-    });
-
-    if (user) {
-      const authenticated = await user.authenticate(credentials.pass);
-      if (authenticated) {
-        console.log(`Authentication successful for username: ${user.emailAddress}`);
-        req.currentUser = user;
-      } else {
-        message = `Authentication failure for username: ${user.emailAddress}`;
-      }
-    } else {
-      message = `User not found for username: ${credentials.name}`;
-    }
-  } else {
-    message = 'Auth header not found';
-  }
-
-  if (message) {
-    console.warn(message);
-    res.status(401).json({ message: 'Access Denied' });
-  } else {
-    next();
-  }
-};
-
-function requireHeader(req) {
-  const authorization = req.headers.authorization;
-  if (authorization) {
-    const base64Credentials = authorization.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii').split(':');
-    return {
-      name: credentials[0],
-      pass: credentials[1],
-    };
-  }
-  return null;
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-module.exports = { authenticateUser };
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
